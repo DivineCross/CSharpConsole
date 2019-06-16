246f397412fb0be30dc138fcde9b3cadfd6b07fb
DivineCross <DivineCross@users.noreply.github.com>
2019-06-16 21:13:22 +0800

    Add console project

---
+++ Validator/console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using ConsoleApplication.Validator;
+using ConsoleApplication.Validator.Extensions;
+
+namespace ConsoleApplication
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+        }
+    }
+}
