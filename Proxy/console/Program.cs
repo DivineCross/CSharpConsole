a0d274e33b57cd2e8c084911a4260a819e7fa26f
DivineCross <DivineCross@users.noreply.github.com>
2019-06-23 13:04:07 +0800

    Add project 'Proxy'

---
+++ Proxy/console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+        }
+    }
+}
