c52913014118b69b6a1499b95d98ddda747181af
DivineCross <DivineCross@users.noreply.github.com>
2019-06-23 18:11:04 +0800

    Add examples in 'Main'

--- Proxy/console/Program.cs
+++ Proxy/console/Program.cs
@@ -1,12 +1,39 @@
 using System;
-using System.Collections.Generic;
 
 namespace Application
 {
+    using DataAccessor;
+
     public static class Program
     {
         public static void Main()
         {
+            var owner = Accessor.Create<Owner>();
+            var cat = Accessor.Create<Cat>();
+            var dog = Accessor.Create<Dog>();
+            var lion = Accessor.Create<Lion>();
+
+            owner.Select(94, 87).Show();
+            owner.Delete(94).Show();
+            owner.Delete(87).Show();
+            owner.Update(94, "I am the owner of these pets.").Show();
+
+            cat.Select(33).Show();
+            cat.Delete(333).Show();
+
+            dog.Select(44).Show();
+            dog.Delete(444).Show();
+
+            lion.Select(1, 2, 3, 4, 5).Show();
+            lion.Delete(1, 2, 3).Show();
         }
+
+        private static void Show(this int[] rows) =>
+            Show(rows?.Length);
+
+        private static void Show(this int? count) =>
+            Console.WriteLine(count.HasValue
+                ? $"[  Out  ] {count} rows are affected.\n"
+                : $"[  Out  ] NO OPERATION!\n");
     }
 }
