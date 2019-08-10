c3e4f867910d03177512ca030f9d44f9121dc743
DivineCross <DivineCross@users.noreply.github.com>
2019-08-11 00:12:32 +0800

    Remove unused using directives

--- Validator/console/Validator/Extensions/Internal/MessageExtensions.cs
+++ Validator/console/Validator/Extensions/Internal/MessageExtensions.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using FluentValidation.Results;
-using FluentValidation.Validators;
-
 namespace ConsoleApplication.Validator.Extensions.Internal
 {
     public static class MessageExtensions
